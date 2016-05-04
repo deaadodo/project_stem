7c83ec3664ee81b5fb89d1234b2565c5b35eff60
sanaa <engsanaa2016@gmail.com>
2016-05-04 14:10:45 +0200

    add aboutus

---
+++ aboutus.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>من نحن</title>
+
+		
+       <link rel="stylesheet" href="css/css/bootstrap.min.css">
+      <link href="css/css/styles.css" rel="stylesheet">
+
+<style>
+
+header.about_us h1 {
+    font: normal normal 200 50px/150px 'Fira Sans', sans-serif;
+    text-align: center;
+    margin-top: 50px;
+    color: #fff;-->
+}
+header.about_us h1 span {
+    position: relative; 
+    top: -15px;
+}
+
+
+#about {
+    padding-bottom: 130px;
+}
+#about p {
+    padding-bottom: 30px;
+    text-align: center;
+}
+#about .f-box {
+    transition: all .5s;
+    background: transparent;
+    padding: 40px 0 40px 0;
+    margin-bottom: 0px;
+    margin-top: 0px;
+}
+#about .f-box:hover {
+    padding: 0;
+    color:#0FC;
+}
+#about .f-box .f-box-inner {
+    box-shadow: inset 0px 0px 0 10px #cecece;
+    border-bottom: 10px solid #cecece;
+    border-top: 10px solid #cecece;
+    transition: all .5s;
+    background: #fff;
+    padding: 20px;
+    margin: auto;
+}
+#about .f-box:hover .f-box-inner {
+    border-bottom: 10px solid #3d799f;
+    border-top: 10px solid #3d799f;
+    padding: 60px 20px 60px 20px;
+    background:#9FF;
+}
+#about .f-box p {
+    max-width: 250px;
+    min-height: 260px;
+    line-height: 32px;
+    padding-top: 30px;
+    font-size: 25px;
+    margin: auto;
+}
+#about .f-box .f-box-icon {
+    text-align: center;
+    padding-top: 50px;
+}
+header.about_us{
+background-image:url(img/img/he.jpg);
+border-radius:50px;}
+</style>
+</head>
+<body>
+<header>
+<?php
+ include"nav_part.php";
+?>
+</header>
+
+  <section id="about">
+            <div class="container-fluid">
+                <header class="about_us">
+                    <h1 class="text-center"><span>...</span> من نحن <span>...</span></h1>
+                </header>
+                <div class="col-sm-12">
+                    <div class="col-sm-6 col-lg-3 f-box">
+                        <div class="f-box-inner">
+                            <div class="f-box-icon">
+                           
+                            </div>
+                            <p>
+                                <br />
+                         . من خلال تسجيل اولياء الامور سيتمكن اولياء الامور من متتابعة ابنائهم ومستواهم التحصيلى  من خلال الندوات التى سنعدها
+                            
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3 f-box">
+                        <div class="f-box-inner">
+                            <div class="f-box-icon">
+                           
+                            </div>
+                            <p>**stem تطبيق**<br />
+ من خلال التطبيق ستتمكن من تحصيل الدرجات النهائية  التى ستؤهلك لدخول مدرسة المتفوقين للعلوم والتكنولوجيا    
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3 f-box">
+                        <div class="f-box-inner">
+                            <div class="f-box-icon">
+                        
+                            </div>
+                            <p>
+                           ** stem تطبيق ** <br />
+يعد التطبيق المواد التعليمية الاساسية والتى تؤهل لدخول مدرسة المتفوقين للعلوم والتكنولوجيا
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3 f-box">
+                        <div class="f-box-inner">
+                            <div class="f-box-icon">
+                            
+                            </div>
+                            <p>
+                               **stem تطبيق**<br />
+يوفر لك امكانية   استذكار موادك العلمية الاساسية  كالعلوم والرياضيات والانجليزية ويوفر لك اجراء اختبارات  بعد كل وحدة دراسية
+                               
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+          <?php
+ include"footer.php";
+?>
+         <script src="js/js/jquery.min.js"></script>
+      <script src="js/js/bootstrap.min.js"></script>
+      
+</body>
+</html>
